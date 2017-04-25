6ba45a80276d96aeaec33798dc5fe84bb05f6222
chewinggumk <chewinggumking@gmail.com>
2017-04-26 02:10:43 +0530

    -a

---
+++ shortener/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class KirrURL(models.Model):
+    url = models.CharField(max_length = 220,)
+    shortcode =  models.CharField(max_length = 15, unique = True,)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    #shortcode =  models.CharField(max_length = 15, null = true,)
+
+    def save(self, *args, **kwargs):
+        super (KirrURL, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.url,)
+
+    def __unicode__(self):
+        return str(self.url)
