31df0fe088dd605639bfc16847eff99fb30bdd91
chewinggumking <chewinggumking@gmail.com>
2017-04-26 19:31:07 +0530

    Video 20 - Regex and URLS

--- shortener/models.py
+++ shortener/models.py
@@ -1,10 +1,10 @@
-
+from django.conf import settings
 from django.db import models
 
 #Utilities
 from .utils import code_generator, create_shortcode
 
-
+SHORTCODE_MAX = getattr (settings, "SHORTCODE_MAX", 15)
 # Create your models here.
 
 
@@ -29,7 +29,7 @@ class KirrURLManager(models.Manager):
 
 class KirrURL(models.Model):
     url = models.CharField(max_length = 220,)
-    shortcode =  models.CharField(max_length = 15, unique = True, blank=True,)
+    shortcode =  models.CharField(max_length = SHORTCODE_MAX, unique = True, blank=True,)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
@@ -40,6 +40,7 @@ class KirrURL(models.Model):
         if self.shortcode is None or self.shortcode =="":
             self.shortcode = create_shortcode(self)
         super (KirrURL, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return str(self.url)
