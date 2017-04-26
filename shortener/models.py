0985af4250571b05b70a791757944f2e14c30df1
chewinggumk <chewinggumking@gmail.com>
2017-04-26 15:07:26 +0530

    Model Manager Training

--- shortener/models.py
+++ shortener/models.py
@@ -1,22 +1,41 @@
-import random
-import string
+
 from django.db import models
+
+#Utilities
+from .utils import code_generator, create_shortcode
 
 #"TD110 15-50 start"
 # Create your models here.
 
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(KirrURLManager,self).all(*args,**kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
+
 
 class KirrURL(models.Model):
     url = models.CharField(max_length = 220,)
-    shortcode =  models.CharField(max_length = 15, unique = True,)
+    shortcode =  models.CharField(max_length = 15, unique = True, blank=True,)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
     #shortcode =  models.CharField(max_length = 15, null = true,)
+    objects = KirrURLManager()
 
     def save(self, *args, **kwargs):
-        self.shortcode=code_generator()
+        if self.shortcode is None or self.shortcode =="":
+            self.shortcode = create_shortcode(self)
         super (KirrURL, self).save(*args, **kwargs)
 
     def __str__(self):
