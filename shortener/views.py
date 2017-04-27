c9cf4157c040a4d42420624b9e68bd2240cbfefe
chewinggumking <chewinggumking@gmail.com>
2017-04-27 17:54:21 +0530

    Query Database

--- shortener/views.py
+++ shortener/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
-
+from .models import KirrURL
 # Create your views here.
 def kirr_redirect_view(request,shortcode=None, *args, **kwargs):
-    print (shortcode)
-    return HttpResponse("Hello {sc}".format(sc=shortcode))
+    # obj = KirrURL.objects.get(shortcode=shortcode)
+    # try:
+    #     obj = KirrURL.objects.get(shortcode=shortcode)
+    # except:
+    #     obj = KirrURL.objects.all().first()
+    obj_url = None
+    qs = KirrURL.objects.filter(shortcode__iexact=shortcode)
+    if qs.exists() and qs.count()==1:
+        obj = qs.first()
+        obj_url=obj.url
+    return HttpResponse("Hello {sc}".format(sc=obj_url))
+
 
 
 class KirrCBView(View):
