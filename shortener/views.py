17910a7fb1732f736290257c66acd5e9b700400c
chewinggumking <chewinggumking@gmail.com>
2017-04-27 20:02:00 +0530

    Video 24 of Trydjano 1.10 finished

--- shortener/views.py
+++ shortener/views.py
@@ -1,25 +1,37 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import KirrURL
 # Create your views here.
+def kirr_redirect_view(request,shortcode=None, *args, **kwargs):
+    print (shortcode)
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    print(obj.shortcode)
+    return HttpResponseRedirect(obj.url)
+
+
+
+class KirrCBView(View):
+    def get (self, request,shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        return HttpResponseRedirect(obj.url)
+
+    def post (self, request, *args, **kwargs):
+        return HttpResponse()
+
+'''
 def kirr_redirect_view(request,shortcode=None, *args, **kwargs):
     # obj = KirrURL.objects.get(shortcode=shortcode)
     # try:
     #     obj = KirrURL.objects.get(shortcode=shortcode)
     # except:
     #     obj = KirrURL.objects.all().first()
-    obj_url = None
-    qs = KirrURL.objects.filter(shortcode__iexact=shortcode)
-    if qs.exists() and qs.count()==1:
-        obj = qs.first()
-        obj_url=obj.url
-    return HttpResponse("Hello {sc}".format(sc=obj_url))
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
 
-
-
-class KirrCBView(View):
-    def get (self, request,shortcode=None, *args, **kwargs):
-        print (shortcode)
-        return HttpResponse("Hello CBV {sc}".format(sc=shortcode))
+    # qs = KirrURL.objects.filter(shortcode__iexact=shortcode)
+    # if qs.exists() and qs.count()==1:
+    #     obj = qs.first()
+    #     obj_url=obj.url
+    return HttpResponse("Hello {sc}".format(sc=obj.url))
+ '''
